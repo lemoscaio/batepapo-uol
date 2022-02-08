55bce21e26e78566f0a0471efb956e2a015b30a7
lemoscaio <caio_lemos94@hotmail.com>
2022-02-08 04:56:27 -0300

    Anima o menu lateral e adiciona função que impede animações ao carregar a página

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
+// Definição das funções
+
 function abrirMenuLateral() {
     const menuLateralEl = document.querySelector(".menu-lateral");
-    const divEscurecidaEl = menuLateralEl.parentNode
+    const divEscurecidaEl = document.querySelector(".escurecer-fundo")
 
     menuLateralEl.classList.remove("escondido")
     divEscurecidaEl.classList.remove("escondido")
 }
 
 function fecharMenuLateral(divEscurecidaEl) {
-    const menuLateralEl = divEscurecidaEl
+    const menuLateralEl = document.querySelector(".menu-lateral")
 
     menuLateralEl.classList.add("escondido")
     divEscurecidaEl.classList.add("escondido")
 }
+
+function prevenirAnimacaoAoCarregar() {
+    document.body.className="";
+}
+
+// Inicialização das funções
+setTimeout(prevenirAnimacaoAoCarregar,100);

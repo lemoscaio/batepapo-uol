581142ce6dded85ad3d3b46ae609d5d7ebc1943b
lemoscaio <caio_lemos94@hotmail.com>
2022-02-10 04:13:24 -0300

    Adiciona tela de login, validação de cadastro e modifica ordem de execução de funções

--- main.js
+++ main.js
@@ -1,21 +1,57 @@
-// function cadastrarUsuario() {
-//     const usuario = {
-//         name: "Caio"
-//     }
-//     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", usuario);
-//     promise.then(cadastrarUsuarioOK);
-//     promise.catch(cadastrarUsuarioFalhou)
-// }
+let usuario = {
+    // name: "youtube.com/graduacaonerd"
+};
+let intervaloBuscarMensagens = null;
+let intervaloVerificarConexao = null;
 
-// function cadastrarUsuarioOK(response) {
-//     console.log(response)
-//     // console.log("Ok, cadastrado!")
-// }
+function cadastrarUsuario() {
+    const inputEl = document.querySelector(".tela-login__input")
+    usuario.name = inputEl.value
 
-// function cadastrarUsuarioFalhou(response) {
-//     // console.log(response)
-//     // console.log("Deu ruim!")
-// }
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", usuario);
+
+    promise.then(cadastrarUsuarioOK);
+    promise.catch(cadastrarUsuarioFalhou);
+}
+
+
+function cadastrarUsuarioOK(response) {
+    const telaLoginEl = document.querySelector(".tela-login")
+    telaLoginEl.classList.add("escondido")
+
+    buscarParticipantes()
+
+}
+
+function cadastrarUsuarioFalhou(response) {
+    if (usuario.name == null || usuario.name == "") {
+        const telaLoginAreaErroEl = document.querySelector(".tela-login__area-erro")
+        telaLoginAreaErroEl.innerHTML = ""
+        telaLoginAreaErroEl.innerHTML += `<div class="tela-login__area-erro"><p class="tela-login__mensagem-erro">Digite um nome de usuário.</p></div>`
+        console.log(usuario.name)
+    }
+    else {
+        const telaLoginAreaErroEl = document.querySelector(".tela-login__area-erro")
+        telaLoginAreaErroEl.innerHTML = ""
+        telaLoginAreaErroEl.innerHTML += `<div class="tela-login__area-erro"><p class="tela-login__mensagem-erro">Usuário ${usuario.name} já existe! Tente outro nome.</p></div>`
+    }
+}
+
+function verificarConexaoUsuario() {
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", usuario);
+    promise.then(verificarConexaoUsuarioOK);
+    promise.catch(verificarConexaoUsuarioFalhou)
+}
+
+function verificarConexaoUsuarioOK(response) {
+    console.log(response)
+    console.log("Ok, continua conectado!")
+}
+
+function verificarConexaoUsuarioFalhou(response) {
+    console.log(response)
+    console.log("Deu ruim e o usuário não está mais conectado!")
+}
 
 function buscarMensagens() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
@@ -24,6 +60,8 @@ function buscarMensagens() {
 
 function imprimirMensagens(response) {
     const mensagens = response.data;
+    const mensagensEl = document.querySelector(".mensagens");
+    mensagensEl.innerHTML = "";
 
     for (let i = 0; i < mensagens.length; i++) {
         const mensagem = mensagens[i];
@@ -32,7 +70,6 @@ function imprimirMensagens(response) {
         <p class="mensagem_conteudo"><span class="mensagem__horario">(${mensagem.time})</span> <span class="mensagem__nome">${mensagem.from}</span> para <span class="mensagem__nome">${mensagem.to}</span>: ${mensagem.text}</p>
         </article>`;
 
-        const mensagensEl = document.querySelector(".mensagens");
         mensagensEl.innerHTML += templateMensagem;
     }
 }
@@ -44,18 +81,16 @@ function buscarParticipantes() {
 
 function imprimirParticipantes(response) {
     const participantes = response.data
-    console.log(participantes)
 
     const listaUsuariosEl = document.querySelector(".menu-lateral__lista.usuarios")
-    console.log(listaUsuariosEl)
 
-    for(let i = 0 ; i < participantes.length ; i++) {
-        participante = participantes[i]
+    for (let i = 0; i < participantes.length; i++) {
+        const participante = participantes[i]
+
         const templateUsuarioLista = `<li class="menu-lateral__item usuario">${participante.name}</li>`
-        
+
         listaUsuariosEl.innerHTML += templateUsuarioLista
     }
-
 }
 
 // Definição das funções
@@ -80,7 +115,7 @@ function prevenirAnimacaoAoCarregar() {
 }
 
 // Inicialização das funções
-setTimeout(prevenirAnimacaoAoCarregar, 100);
-// cadastrarUsuario();
-buscarParticipantes()
-buscarMensagens(); 
+// setTimeout(prevenirAnimacaoAoCarregar, 100);
+buscarMensagens();
+intervaloBuscarMensagens = setInterval(buscarMensagens, 5000);
+intervaloVerificarConexao = setInterval(verificarConexaoUsuario, 5000);

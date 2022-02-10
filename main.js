76feb6c3f46d3218f6bad0335088752a503be18c
lemoscaio <caio_lemos94@hotmail.com>
2022-02-10 01:33:20 -0300

    Implementa função: Busca mensagens via API e imprime na tela.

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function buscarMensagens() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+    promise.then(imprimirMensagens)
+}
+
+function imprimirMensagens(response) {
+    const mensagens = response.data
+
+    for (let i = 0; i < mensagens.length; i++) {
+        const mensagem = mensagens[i]
+
+        const templateMensagem = `<article class="mensagem ${mensagem.type}">
+        <p class="mensagem_conteudo"><span class="mensagem__horario">(${mensagem.time})</span> <span class="mensagem__nome">${mensagem.from}</span> para <span class="mensagem__nome">${mensagem.to}</span>: ${mensagem.text}</p>
+        </article>`
+
+        const mensagensEl = document.querySelector(".mensagens")
+        mensagensEl.innerHTML += templateMensagem
+    }
+}
+
 // Definição das funções
 
 function abrirMenuLateral() {
@@ -16,8 +36,9 @@ function fecharMenuLateral(divEscurecidaEl) {
 }
 
 function prevenirAnimacaoAoCarregar() {
-    document.body.className="";
+    document.body.className = "";
 }
 
 // Inicialização das funções
-setTimeout(prevenirAnimacaoAoCarregar,100);
+setTimeout(prevenirAnimacaoAoCarregar, 100);
+buscarMensagens(); 
